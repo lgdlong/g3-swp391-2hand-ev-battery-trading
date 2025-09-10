aba19c0113030edfac2055a3ff2ca39053ddb102
Phung Luu Hoang Long <phungluuhoanglong@gmail.com>
2025-09-11 03:06:24 +0700

    Add `accounts` module with entity, service, controller, DTOs, and enums
    - Implement `Account` entity with validation and TypeORM integration.
    - Add `AccountsService` with CRUD methods.
    - Create `AccountsController` with endpoints for account management.
    - Add DTOs for creating and updating accounts with validation.
    - Define `AccountRole` and `AccountStatus` enums.

---
+++ apps/api/src/modules/accounts/accounts.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@nestjs/common';
+import { CreateAccountDto } from './dto/create-account.dto';
+import { UpdateAccountDto } from './dto/update-account.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Account } from './entities/account.entity';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class AccountsService {
+  constructor(
+    @InjectRepository(Account)
+    private readonly repo: Repository<Account>,
+    private readonly configService: ConfigService,
+  ) {}
+  create(createAccountDto: CreateAccountDto) {
+    return 'This action adds a new account';
+  }
+
+  findAll() {
+    return `This action returns all accounts`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} account`;
+  }
+
+  findByEmail(email: string) {
+    return `This action returns a account with email ${email}`;
+  }
+
+  update(id: number, updateAccountDto: UpdateAccountDto) {
+    return `This action updates a #${id} account`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} account`;
+  }
+}
