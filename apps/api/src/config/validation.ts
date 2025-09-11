9641f6eddba6ef51cb39aba13520f2723e4b5497
Phung Luu Hoang Long <phungluuhoanglong@gmail.com>
2025-09-11 12:28:14 +0700

    refactor: lint for readability

--- apps/api/src/config/validation.ts
+++ apps/api/src/config/validation.ts
@@ -8,12 +8,10 @@ export interface EnvVars {
   DB_NAME: string;
 }
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-export const envValidationSchema: Joi.ObjectSchema<EnvVars> =
-  Joi.object<EnvVars>({
-    DB_HOST: Joi.string().required(),
-    DB_PORT: Joi.number().default(5432),
-    USERNAME: Joi.string().required(),
-    PASSWORD: Joi.string().min(1).required(),
-    DB_NAME: Joi.string().required(),
-  });
+export const envValidationSchema: Joi.ObjectSchema<EnvVars> = Joi.object<EnvVars>({
+  DB_HOST: Joi.string().required(),
+  DB_PORT: Joi.number().default(5432),
+  USERNAME: Joi.string().required(),
+  PASSWORD: Joi.string().min(1).required(),
+  DB_NAME: Joi.string().required(),
+});
