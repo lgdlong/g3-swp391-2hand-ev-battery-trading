979b409d01005c581a74b78ae1e3c032afc9b821
Phung Luu Hoang Long <phungluuhoanglong@gmail.com>
2025-09-11 23:21:08 +0700

    refactor: rename Vietnamese phone number regex constant for consistency

--- apps/web/utils/regex.ts
+++ apps/web/utils/regex.ts
@@ -1,3 +1,3 @@
 // Vietnamese phone number regex pattern
-export const vietnamesePhoneRegex =
+export const VIETNAMESE_PHONE_REGEX =
   /^(\+84|84|0)(3[2-9]|5[25689]|7[06-9]|8[1-689]|9[0-46-9])[0-9]{7}$/;
