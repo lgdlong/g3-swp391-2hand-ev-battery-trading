5e229f4d4cf7ae491c32ec33666cf9fcd3304f4b
Phung Luu Hoang Long <phungluuhoanglong@gmail.com>
2025-09-13 16:07:57 +0700

    feat(validation): enable global validation with custom options in main.ts

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,9 +2,22 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DEFAULT_FRONTEND_URL, DEFAULT_PORT_BACKEND } from './shared/constants';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Bật validation cho toàn bộ ứng dụng
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Loại bỏ field thừa không khai báo trong DTO
+      forbidNonWhitelisted: false, // (optional) bật lên thì sẽ throw nếu có field lạ
+      transform: true, // Tự động transform primitive (string -> number, …)
+      transformOptions: {
+        enableImplicitConversion: true, // cho phép auto convert nếu type match
+      },
+    }),
+  );
 
   // Lấy ConfigService từ app context
   const configService = app.get(ConfigService);
