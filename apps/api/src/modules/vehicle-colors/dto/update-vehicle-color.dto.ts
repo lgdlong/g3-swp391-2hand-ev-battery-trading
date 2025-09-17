43dc38d3d281b6d73e80ace5576021d82eb334f7
Phung Luu Hoang Long <phungluuhoanglong@gmail.com>
2025-09-17 23:36:28 +0700

    chore(api): remove vehicle colors module and using enums just for more simple

--- apps/api/src/modules/vehicle-colors/dto/update-vehicle-color.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateVehicleColorDto } from './create-vehicle-color.dto';
-
-export class UpdateVehicleColorDto extends PartialType(CreateVehicleColorDto) {}
--- apps/api/src/modules/vehicle-colors/vehicle-colors.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { VehicleColorsService } from './vehicle-colors.service';
-import { VehicleColorsController } from './vehicle-colors.controller';
-
-@Module({
-  controllers: [VehicleColorsController],
-  providers: [VehicleColorsService],
-})
-export class VehicleColorsModule {}
