4222e6465162097e2881f10f57d344ca9d753fa6
F15 <duongquocthai368@gmail.com>
2025-10-02 05:22:16 +0700

    refactor(PostBookmark): enhance entity structure with proper column names and relationships

--- apps/api/src/modules/post_bookmarks/entities/post_bookmark.entity.ts
+++ apps/api/src/modules/post_bookmarks/entities/post_bookmark.entity.ts
@@ -1,19 +1,28 @@
-import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, Unique } from "typeorm";
+import { Account } from "src/modules/accounts/entities/account.entity";
+import { Post } from "src/modules/posts/entities/post.entity";
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn, Unique } from "typeorm";
 
 
 @Entity('post_bookmarks')
 @Unique(['postId', 'accountId'])
 export class PostBookmark {
-  @PrimaryGeneratedColumn()
+    @PrimaryGeneratedColumn({ name: 'id' })
   id!: number;
 
-  @Column()
+  @Column({ name: 'account_id', type: 'int' })
   accountId!: number;
 
-  @Column()
+  @ManyToOne(() => Account, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'account_id' })  // FK column snake_case
+  account!: Account;
+
+  @Column({ name: 'post_id', type: 'int' })
   postId!: number;
 
+  @ManyToOne(() => Post, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'post_id' })     // FK column snake_case
+  post!: Post;
 
-  @CreateDateColumn({ type: 'timestamp' })
+  @CreateDateColumn({ name: 'created_at', type: 'timestamptz' })
   createdAt!: Date;
 }
