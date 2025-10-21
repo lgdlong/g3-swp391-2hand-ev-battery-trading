2daeb5be23432e80f64546351668b08c2ea93a85
Phung Luu Hoang Long <phungluuhoanglong@gmail.com>
2025-10-22 01:42:55 +0700

    refactor: enhance PayosService with return types for improved type safety and clarity

--- apps/api/src/modules/payos/payos.service.ts
+++ apps/api/src/modules/payos/payos.service.ts
@@ -1,5 +1,10 @@
 import { Injectable } from '@nestjs/common';
-import { CreatePayosDto } from './dto';
+import {
+  CancelPaymentDto,
+  CreatePayosDto,
+  PayosCreatePaymentResponse,
+  PayosGetPaymentResponse,
+} from './dto';
 import { createHmac } from 'crypto';
 import axios from 'axios';
 
@@ -18,7 +23,7 @@ export class PayosService {
     return createHmac('sha256', this.payosChecksumKey).update(str).digest('hex');
   }
 
-  async create(createPayOSDto: CreatePayosDto) {
+  async create(createPayOSDto: CreatePayosDto): Promise<PayosCreatePaymentResponse> {
     const signature = this.signcreate(createPayOSDto);
     console.log('Creating PayO payment request with data:', { ...createPayOSDto, signature });
     const res = await axios.post(
@@ -39,18 +44,17 @@ export class PayosService {
     return res.data;
   }
 
-  async find(orderCode: number | string) {
+  async find(orderCode: number | string): Promise<PayosGetPaymentResponse> {
     const res = await axios.get(`${this.payosBaseUrl}/v2/payment-requests/${orderCode}`, {
       headers: {
         'x-client-id': this.payosClientId,
         'x-api-key': this.payosApiKey,
       },
     });
-    const payosData = res.data;
-    return payosData;
+    return res.data;
   }
 
-  async cancel(orderCode: number, reason?: string) {
+  async cancel(orderCode: number, reason?: string): Promise<CancelPaymentDto> {
     const res = await axios.post(
       `${this.payosBaseUrl}/v2/payment-requests/${orderCode}/cancel`,
       {
